eaecaa59cde3ec2909a77c0383a5894e04e275b6
Szalai László <szalai.laszlo@tudaskozpont-pecs.hu>
2018-08-28 11:23:41 +0200

    Template helperhez és a szeparált sablonokhoz szükséges módosítások

--- src/PhpRenderer.php
+++ src/PhpRenderer.php
@@ -19,172 +19,233 @@ use Psr\Http\Message\ResponseInterface;
  */
 class PhpRenderer
 {
-    /**
-     * @var string
-     */
-    protected $templatePath;
+	/**
+	 * @var string
+	 */
+	protected $templatePath;
 
-    /**
-     * @var array
-     */
-    protected $attributes;
+	/**
+	 * @var array
+	 */
+	protected $attributes;
 
-    /**
-     * SlimRenderer constructor.
-     *
-     * @param string $templatePath
-     * @param array $attributes
-     */
-    public function __construct($templatePath = "", $attributes = [])
-    {
-        $this->templatePath = rtrim($templatePath, '/\\') . '/';
-        $this->attributes = $attributes;
-    }
+	/**
+	 * SlimRenderer constructor.
+	 *
+	 * @param string $templatePath
+	 * @param array $attributes
+	 */
+	public function __construct($templatePath = "", $attributes = [])
+	{
+		$this->templatePath = rtrim($templatePath, '/\\') . '/';
+		$this->attributes = $attributes;
+	}
 
-    /**
-     * Render a template
-     *
-     * $data cannot contain template as a key
-     *
-     * throws RuntimeException if $templatePath . $template does not exist
-     *
-     * @param ResponseInterface $response
-     * @param string             $template
-     * @param array              $data
-     *
-     * @return ResponseInterface
-     *
-     * @throws \InvalidArgumentException
-     * @throws \RuntimeException
-     */
-    public function render(ResponseInterface $response, $template, array $data = [])
-    {
-        $output = $this->fetch($template, $data);
+	/**
+	 * Render a template
+	 *
+	 * $data cannot contain template as a key
+	 *
+	 * throws RuntimeException if $templatePath . $template does not exist
+	 *
+	 * @param ResponseInterface $response
+	 * @param string             $template
+	 * @param array              $data
+	 *
+	 * @return ResponseInterface
+	 *
+	 * @throws \InvalidArgumentException
+	 * @throws \RuntimeException
+	 */
+	public function render(ResponseInterface $response, $template, array $data = [])
+	{
+		$output = $this->fetch($template, $data);
+		if (
+			!empty($_COOKIE['templateHelper']) && $_COOKIE['templateHelper'] == 1
+			&&
+			(
+				empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+				||
+				strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest'
+			)
+		){
+			$output .= rv($this->used_templates, '', 4);
+			//$output .= rv($_COOKIE);
+		}
 
-        $response->getBody()->write($output);
+		$response->getBody()->write($output);
 
-        return $response;
-    }
+		return $response;
+	}
 
-    /**
-     * Get the attributes for the renderer
-     *
-     * @return array
-     */
-    public function getAttributes()
-    {
-        return $this->attributes;
-    }
+	/**
+	 * Get the attributes for the renderer
+	 *
+	 * @return array
+	 */
+	public function getAttributes()
+	{
+		return $this->attributes;
+	}
 
-    /**
-     * Set the attributes for the renderer
-     *
-     * @param array $attributes
-     */
-    public function setAttributes(array $attributes)
-    {
-        $this->attributes = $attributes;
-    }
+	/**
+	 * Set the attributes for the renderer
+	 *
+	 * @param array $attributes
+	 */
+	public function setAttributes(array $attributes)
+	{
+		$this->attributes = $attributes;
+	}
 
-    /**
-     * Add an attribute
-     *
-     * @param $key
-     * @param $value
-     */
-    public function addAttribute($key, $value) {
-        $this->attributes[$key] = $value;
-    }
+	/**
+	 * Add an attribute
+	 *
+	 * @param $key
+	 * @param $value
+	 */
+	public function addAttribute($key, $value) {
+		$this->attributes[$key] = $value;
+	}
 
-    /**
-     * Retrieve an attribute
-     *
-     * @param $key
-     * @return mixed
-     */
-    public function getAttribute($key) {
-        if (!isset($this->attributes[$key])) {
-            return false;
-        }
+	/**
+	 * Retrieve an attribute
+	 *
+	 * @param $key
+	 * @return mixed
+	 */
+	public function getAttribute($key) {
+		if (!isset($this->attributes[$key])) {
+			return false;
+		}
 
-        return $this->attributes[$key];
-    }
+		return $this->attributes[$key];
+	}
 
-    /**
-     * Get the template path
-     *
-     * @return string
-     */
-    public function getTemplatePath()
-    {
-        return $this->templatePath;
-    }
+	/**
+	 * Get the template path
+	 *
+	 * @return string
+	 */
+	public function getTemplatePath()
+	{
+		return $this->templatePath;
+	}
 
-    /**
-     * Set the template path
-     *
-     * @param string $templatePath
-     */
-    public function setTemplatePath($templatePath)
-    {
-        $this->templatePath = rtrim($templatePath, '/\\') . '/';
-    }
+	/**
+	 * Set the template path
+	 *
+	 * @param string $templatePath
+	 */
+	public function setTemplatePath($templatePath)
+	{
+        	$this->templatePath = rtrim($templatePath, '/\\') . '/';
+	}
 
-    /**
-     * Renders a template and returns the result as a string
-     *
-     * cannot contain template as a key
-     *
-     * throws RuntimeException if $templatePath . $template does not exist
-     *
-     * @param $template
-     * @param array $data
-     *
-     * @return mixed
-     *
-     * @throws \InvalidArgumentException
-     * @throws \RuntimeException
-     */
-    public function fetch($template, array $data = []) {
-        if (isset($data['template'])) {
-            throw new \InvalidArgumentException("Duplicate template key found");
-        }
+	/**
+	 * Renders a template and returns the result as a string
+	 *
+	 * cannot contain template as a key
+	 *
+	 * throws RuntimeException if $templatePath . $template does not exist
+	 *
+	 * @param $template
+	 * @param array $data
+	 *
+	 * @return mixed
+	 *
+	 * @throws \InvalidArgumentException
+	 * @throws \RuntimeException
+	 */
+	public function fetch($template, array $data = []) {
+	
+		$bt = debug_backtrace();
+		
+		//$this->used_templates[] = $template.'|'.$bt[0]['file'].'|'.$bt[0]['line'];
+		
+		$this->used_templates[$bt[0]['file'].'|'.$bt[0]['line']] = $template;
+		if (isset($data['template'])) {
+			throw new \InvalidArgumentException("Duplicate template key found");
+		}
 
-        if (!is_file($this->templatePath . $template)) {
-            throw new \RuntimeException("View cannot render `$template` because the template does not exist");
-        }
+		$real_template_path = $this->templatePath;
+		if (in_array($template[0], array(DIRECTORY_SEPARATOR, '\\', '/'))) {
+			$tmp = str_replace(array('\\', '/'), array(DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR), $this->templatePath);
+			$tmp = rtrim($tmp.DIRECTORY_SEPARATOR, '\\/');
+			$tmp_array = explode(DIRECTORY_SEPARATOR, $tmp);
+			array_pop($tmp_array);
+			$real_template_path = implode(DIRECTORY_SEPARATOR, $tmp_array);
+
+		} else {
+			$real_template_path = '';
+		}
+
+		if (!is_file($real_template_path.$template)) {
+			dve("View cannot render `".$real_template_path."$template` because the template does not exist");
+			throw new \RuntimeException("View cannot render `".$real_template_path."$template` because the template does not exist");
+		}
 
 
-        /*
-        foreach ($data as $k=>$val) {
-            if (in_array($k, array_keys($this->attributes))) {
-                throw new \InvalidArgumentException("Duplicate key found in data and renderer attributes. " . $k);
-            }
-        }
-        */
-        $data = array_merge($this->attributes, $data);
+		/*
+		foreach ($data as $k=>$val) {
+			if (in_array($k, array_keys($this->attributes))) {
+				throw new \InvalidArgumentException("Duplicate key found in data and renderer attributes. " . $k);
+			}
+		}
+		*/
+		$data = array_merge($this->attributes, $data);
 
-        try {
-            ob_start();
-            $this->protectedIncludeScope($this->templatePath . $template, $data);
-            $output = ob_get_clean();
-        } catch(\Throwable $e) { // PHP 7+
-            ob_end_clean();
-            throw $e;
-        } catch(\Exception $e) { // PHP < 7
-            ob_end_clean();
-            throw $e;
-        }
+		try {
+			ob_start();
+			$this->protectedIncludeScope($real_template_path . $template, $data);
+			$output = ob_get_clean();
 
-        return $output;
-    }
+			if (
+				!empty($_COOKIE['templateHelper']) && $_COOKIE['templateHelper'] == 1
+				&&
+				(
+					empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+					||
+					strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest'
+				)
+				&&
+				(!strstr($template, 'layout'.DIRECTORY_SEPARATOR.'body'))
+			){
+				$first_tag = '';
+				if(preg_match('(<(\w+)[^>]*>)', $output, $matches)){
+					if(strstr($matches[0],'class="')){
+						$first_tag = str_replace('class="', 'data-template="'.$template.'" class="templateInfo ', $matches[0]);
+					} else {
+						$first_tag = str_replace('<'.$matches[1], '<'.$matches[1].' data-template="'.$template.'" class="templateInfo" ', $matches[0]);
+					}
+					$output = str_replace($matches[0], $first_tag, $output);
+				} else {
+					$output = '<span data-template="'.$template.'" class="templateInfo">'.$output.'</span>';
+				}
 
-    /**
-     * @param string $template
-     * @param array $data
-     */
-    protected function protectedIncludeScope ($template, array $data) {
-        extract($data);
+				$output = '<!--START '.$this->templatePath.$template.'-->'.$output.'<!-- '.$template.' END-->';
+			}
+
+		} catch(\Throwable $e) { // PHP 7+
+			ob_end_clean();
+			throw $e;
+		} catch(\Exception $e) { // PHP < 7
+			ob_end_clean();
+			throw $e;
+		}
+
+
+
+
+		return $output;
+	}
+
+	/**
+	 * @param string $template
+	 * @param array $data
+	 */
+	protected function protectedIncludeScope ($template, array $data) {
+		extract($data);
         include func_get_arg(0);
-    }
+	}
 }
